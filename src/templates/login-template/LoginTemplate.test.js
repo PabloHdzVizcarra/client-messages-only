7223872e84787f7324936714a89ef187196e5357
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-31 08:25:01 -0500

    test case alert to error data form

--- src/templates/login-template/LoginTemplate.test.js
+++ src/templates/login-template/LoginTemplate.test.js
@@ -1,8 +1,19 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import LoginTemplate from "./LoginTemplate";
+import userEvent from "@testing-library/user-event";
 
 describe("test in LoginTemplate component", () => {
-  test("show error with value error form", () => {
-    render(<LoginTemplate/>)
-  })
-})
+  test("should display an alert when the form data is invalid", async () => {
+    render(<LoginTemplate/>);
+
+    screen.debug();
+    const inputEmail = screen.getAllByRole("textbox")[0];
+    const buttonSubmit = screen.getByRole("button");
+
+    userEvent.type(inputEmail, "error@email");
+    userEvent.click(buttonSubmit);
+
+    const errorAlert = await screen.findByRole("alert-error");
+    expect(errorAlert).toBeInTheDocument();
+  });
+});
