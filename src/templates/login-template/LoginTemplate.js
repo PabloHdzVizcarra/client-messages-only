d06314c909d43a0671691d7bf5fdcd38a688bb02
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-31 08:29:58 -0500

    feat show alert when form data have errors

--- src/templates/login-template/LoginTemplate.js
+++ src/templates/login-template/LoginTemplate.js
@@ -1,7 +1,8 @@
 import React from "react";
-import FormLogin from "../../components/organisms/form-login/FormLogin";
+import FormLogin from "./form-login/FormLogin";
 import { useForm } from "../../hooks/use-form/useForm";
 import { validateLoginData } from "./validate-data-helper";
+import ErrorAlert from "../../components/alerts/ErrorAlert";
 
 function LoginTemplate () {
   const [error, setError] = React.useState({
@@ -16,15 +17,20 @@ function LoginTemplate () {
 
   function handleSubmit (event) {
     event.preventDefault();
-    const { error, message } = validateLoginData(
+    const { error, message: errorMessage } = validateLoginData(
       inputValues.email, inputValues.password,
     );
+    if (error) {
+      setError({ error: true, message: errorMessage });
+      return null;
+    }
 
     console.log(inputValues);
   }
 
   return (
     <>
+      {error.error ? <ErrorAlert text={error.message}/> : null}
       <FormLogin
         handleSubmit={handleSubmit}
         inputValues={inputValues}
--- src/templates/login-template/LoginTemplate.test.js
+++ src/templates/login-template/LoginTemplate.test.js
@@ -6,14 +6,13 @@ describe("test in LoginTemplate component", () => {
   test("should display an alert when the form data is invalid", async () => {
     render(<LoginTemplate/>);
 
-    screen.debug();
     const inputEmail = screen.getAllByRole("textbox")[0];
     const buttonSubmit = screen.getByRole("button");
 
     userEvent.type(inputEmail, "error@email");
     userEvent.click(buttonSubmit);
 
-    const errorAlert = await screen.findByRole("alert-error");
+    const errorAlert = await screen.findByTestId("alert-error");
     expect(errorAlert).toBeInTheDocument();
   });
 });
